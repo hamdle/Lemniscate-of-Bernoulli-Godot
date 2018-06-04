27d957bf7f3236cec93c0ea45cdccb49846b33ee
hamdle <contact@ericmarty.co>
2018-06-04 14:25:38 -0700

    Add lemniscate of bernoulli function

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -3,7 +3,8 @@ extends KinematicBody2D
 var points = Array()
 
 func _ready():
-	lemniscate_of_gerono(-4, 4)
+	# lemniscate_of_gerono(-4, 4)
+	lemniscate_of_bernoulli(-4, 4)
 
 func _draw():
 	for p in points:
@@ -18,4 +19,16 @@ func lemniscate_of_gerono(start, end):
 		new_pos.y = sin(2 * t) / 2
 		print(new_pos * scale)
 		points.append(new_pos * scale)
+		t += 0.1
+	
+func lemniscate_of_bernoulli(start, end):
+	var new_pos = Vector2()
+	var pos_scale = Vector2(100,100)
+	var t = start
+	while t <= end:
+		var scale = 2 / (3 - cos(2 * t))
+		new_pos.x = scale * cos(t)
+		new_pos.y = scale * sin(2 * t) / 2
+		print(new_pos * pos_scale)
+		points.append(new_pos * pos_scale)
 		t += 0.1
